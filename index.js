83e837dabbc5eaf9f90b2c4ed3c795d0c19e05eb
Owethu927 <owethusilela@gmail.com>
2025-02-02 02:03:41 -0600

    added fallback error catch

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+//Getting the photos from unsplash//
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
 .then(response => response.json())
 .then(data => {
     document.body.style.backgroundImage = `url(${data.urls.full})`
-    document.getElementById("author").textContent = `By:${data.user.name}`
+    document.getElementById("author").textContent = `By:${data.user.name}`//Getting the users name by the url
+})
+//Adding the catch any error rejection promise//
+.catch(err => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
 })
