b85d2773c6bdf02e96e56cf2b035d6d2b36583a5
Owethu927 <owethusilela@gmail.com>
2025-02-02 02:40:27 -0600

    Diplay the icon and image

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     console.log(res.status)
     return res.json()
 })
-.then(data => {
-    console.log(data)
+.then(data => { //DISPLAYING THE NAME AND ICON
+    document.getElementById("crypto-top").innerHTML = `
+    <img src=${data.image.small} />
+    <span>${data.name}</span>`
 })
 .catch(err => console.log(err))
